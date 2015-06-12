57a55761980255759dbeb7a9cbae217ad3109795
Hays Clark <hays@omita.com>
2015-06-11 22:30:57 -0700

    adding article love

--- app/models/article.js
+++ app/models/article.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  createAt: DS.attr('date'),
+  createdAt: DS.attr('date'),
   description: DS.attr('string'),
   friend: DS.belongsTo('friend'),
   notes: DS.attr('string'),
--- app/models/friend.js
+++ app/models/friend.js
@@ -2,7 +2,9 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
-  articles: 	 DS.hasMany('article', {async:true}),
+  articles: 	 DS.hasMany('article', { 
+  	async: true
+  }),
   email: 		 DS.attr('string'),
   firstName: 	 DS.attr('string'),
   lastName: 	 DS.attr('string'),
